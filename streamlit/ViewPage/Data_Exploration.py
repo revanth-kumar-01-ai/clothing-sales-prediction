e21f5e3d8bb93df6c7f5250844b68d00146b1a91
revanth-kumar-01-ai <revanthj486@gmail.com>
2025-03-05 01:47:33 +0530

    Streamlit UI

--- streamlit/ViewPage/Data_Exploration.py
+++ streamlit/ViewPage/Data_Exploration.py
@@ -6,8 +6,8 @@ st.title("Data Exploration - Loan")
 
 # Show first 5 rows
 st.subheader("Dataset Describe")
-st.image('Streamlit/assets/describe.png')
+st.image('assets/describe.png')
 
 # Show dataset description
 st.subheader("Dataset head map")
-st.image('Streamlit/assets/heatMap.png')
+st.image('assets/heatMap.png')
---
+++ streamlit/app.py
@@ -0,0 +1,41 @@
+import streamlit as st
+
+st.set_page_config(layout = 'wide', page_title = "Cloths ")
+
+home = st.Page(
+    page = "./ViewPage/Home.py", 
+    title = "Home", 
+    icon = '🏠' 
+)
+
+dataExploration = st.Page(
+    page = "./ViewPage/Data_Exploration.py", 
+    title = "Data Exploration", 
+    icon = '📊' 
+)
+
+modelTraining = st.Page(
+    page = "./ViewPage/Model_Training.py", 
+    title = "Model Training", 
+    icon = '🤖' 
+)
+
+prediction = st.Page(
+    page = "./ViewPage/Prediction.py", 
+    title = "Prediction", 
+    icon = '🎯' 
+)
+
+conclusion = st.Page(
+    page = "./ViewPage/Conclusion.py", 
+    title = "Conclusion", 
+    icon = '🔍' 
+)
+
+st.logo('./assets/logo.png', size = 'large')
+
+pg = st.navigation({
+    "cloths":[home, dataExploration, modelTraining, prediction, conclusion]
+})
+
+pg.run()
