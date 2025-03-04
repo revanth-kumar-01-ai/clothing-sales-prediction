91ff67839503ffb597444d216f623b2e6d5a87f0
revanth-kumar-01-ai <revanthj486@gmail.com>
2025-03-05 01:20:13 +0530

    Streamlit UI

--- app.py
+++ app.py
@@ -3,31 +3,31 @@ import streamlit as st
 st.set_page_config(layout = 'wide', page_title = '👕 Cloth Sales prediction 👕')
 
 home = st.Page(
-    page = "./Streamlit/ViewPage/Home.py", 
+    page = "../clothing-sales-prediction/Streamlit/ViewPage/Home.py", 
     title = "Home", 
     icon = '🏠' 
 )
 
 dataExploration = st.Page(
-    page = "./Streamlit/ViewPage/Data_Exploration.py", 
+    page = "../clothing-sales-prediction/Streamlit/ViewPage/Data_Exploration.py", 
     title = "Data Exploration", 
     icon = '📊' 
 )
 
 modelTraining = st.Page(
-    page = "./Streamlit/ViewPage/Model_Training.py", 
+    page = "../clothing-sales-prediction/Streamlit/ViewPage/Model_Training.py", 
     title = "Model Training", 
     icon = '🤖' 
 )
 
 prediction = st.Page(
-    page = "./Streamlit/ViewPage/Prediction.py", 
+    page = "../clothing-sales-prediction/Streamlit/ViewPage/Prediction.py", 
     title = "Prediction", 
     icon = '🎯' 
 )
 
 conclusion = st.Page(
-    page = "./Streamlit/ViewPage/Conclusion.py", 
+    page = "../clothing-sales-prediction/Streamlit/ViewPage/Conclusion.py", 
     title = "Conclusion", 
     icon = '🔍' 
 )
