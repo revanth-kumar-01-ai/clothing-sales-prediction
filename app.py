657ede4e0a7befb74241d3b018db1788404ee6b6
revanth-kumar-01-ai <revanthj486@gmail.com>
2025-03-05 01:36:21 +0530

    Streamlit UI

--- app.py
+++ app.py
@@ -1,43 +1,42 @@
 import streamlit as st
 
-st.set_page_config(layout = 'wide', page_title = '👕 Cloth Sales prediction 👕')
+st.set_page_config(layout='wide', page_title='👕 Cloth Sales prediction 👕')
 
+# Assuming the files are in the same directory as app.py
 home = st.Page(
-    page = "/Streamlit/ViewPage/Home.py", 
-    title = "Home", 
-    icon = '🏠' 
+    page="./Streamlit/ViewPage/Home.py", 
+    title="Home", 
+    icon='🏠' 
 )
 
 dataExploration = st.Page(
-    page = "../clothing-sales-prediction/Streamlit/ViewPage/Data_Exploration.py", 
-    title = "Data Exploration", 
-    icon = '📊' 
+    page="./Streamlit/ViewPage/Data_Exploration.py", 
+    title="Data Exploration", 
+    icon='📊' 
 )
 
 modelTraining = st.Page(
-    page = "../clothing-sales-prediction/Streamlit/ViewPage/Model_Training.py", 
-    title = "Model Training", 
-    icon = '🤖' 
+     page="./Streamlit/ViewPage/Model_Training.py", 
+    title="Model Training", 
+    icon='🤖' 
 )
 
 prediction = st.Page(
-    page = "../clothing-sales-prediction/Streamlit/ViewPage/Prediction.py", 
-    title = "Prediction", 
-    icon = '🎯' 
+  page="./Streamlit/ViewPage/Prediction.py", 
+    title="Prediction", 
+    icon='🎯' 
 )
 
 conclusion = st.Page(
-    page = "../clothing-sales-prediction/Streamlit/ViewPage/Conclusion.py", 
-    title = "Conclusion", 
-    icon = '🔍' 
+     page="./Streamlit/ViewPage/Conclusion.py", 
+    title="Conclusion", 
+    icon='🔍' 
 )
 
-st.logo('./Streamlit/assets/logo.png', size = 'large')
+st.logo('./Streamlit/assets/logo.png', size='large')
 
 pg = st.navigation({
-    '👕 Cloth Sales prediction 👕':[home, dataExploration, modelTraining, prediction, conclusion],
-    
-
+    '👕 Cloth Sales prediction 👕': [home, dataExploration, modelTraining, prediction, conclusion],
 })
 
 pg.run()
